4783ea56d31827e1272f05ca403f1e6e396a4642
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2025-03-17 15:37:10 +0100

    add job to calculate polygon area

---
+++ src/jobs/PolygonAreaJob.ts
@@ -0,0 +1,19 @@
+import { Job } from './Job';
+import { Task } from '../models/Task';
+import { Feature, Polygon } from 'geojson';
+import * as turf from '@turf/turf';
+
+export class PolygonAreaJob implements Job {
+    async run(task: Task): Promise<number> {
+        console.log(`Calculating polygon area for task ${task.taskId}...`);
+
+        const inputGeometry: Feature<Polygon> = JSON.parse(task.geoJson);
+        const area = turf.area(inputGeometry);
+
+        if (area <= 0) {
+            throw new Error('Invalid area');
+        }
+
+        return area;
+    }
+}
